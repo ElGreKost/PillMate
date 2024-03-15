acc65de7723a10a07cdedd32164c2191f3ec816b
Panagiotis Konstantinos Kakkavas <el20866@mail.ntua.gr>
2024-03-15 12:59:02 +0200

    suggestion for pills ui is ready

--- pillmate-v1.0.2/lib/presentation/select_pill_name/select_pill_name.dart
+++ pillmate-v1.0.2/lib/presentation/select_pill_name/select_pill_name.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:pillmate/core/app_export.dart';
-import 'package:pillmate/widgets/custom_text_field.dart';
-import '../../services/medication_data_provider.dart';
 import 'package:provider/provider.dart';
-
+import '../../services/medication_data_provider.dart';
 import '../../widgets/app_bar/custom_app_bar.dart';
 import '../../widgets/custom_elevated_button.dart';
-import '../pickaudio_screen/pickaudio_screen.dart';
+import '../../widgets/custom_text_field.dart';
 
 class SelectPillName extends StatefulWidget {
   @override
@@ -15,30 +13,118 @@ class SelectPillName extends StatefulWidget {
 }
 
 class _SelectPillNameState extends State<SelectPillName> {
-  var pillNameController = TextEditingController();
+  final List<String> _medicationNames = [
+    'Ibuprofen',
+    'Paracetamol',
+    'Amoxicillin',
+    'Aspirin',
+    // Add more as needed
+  ];
+
+  bool _isValidSelection = false;
+
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            appBar: CustomAppBar(titleText: 'Which medication to add'),
-            body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(children: [
-                CustomTextField(
-                    controller: pillNameController,
-                    name: "Medication Name",
-                    prefixIcon: LineIcons.pills,
-                    inputType: TextInputType.text),
-                CustomElevatedButton(
-                    onPressed: () {
-                      Provider.of<MedicationProvider>(context, listen: false)
-                          .setSelectedPillName(pillNameController.text);
-                      Navigator.pushNamed(context, AppRoutes.selectMedicationTypeScreen);
-                    },
-                    text: 'Next',
-                    width: 100.h),
-              ]),
-            )));
+      child: Scaffold(
+        appBar: CustomAppBar(titleText: 'Select Medication'),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            Autocomplete<String>(
+              optionsMaxHeight: 200,
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text.isEmpty) {
+                  return const Iterable<String>.empty();
+                }
+                return _medicationNames.where((String option) {
+                  return option
+                      .toLowerCase()
+                      .contains(textEditingValue.text.toLowerCase());
+                });
+              },
+              onSelected: (String selection) {
+                setState(() => _isValidSelection = true);
+                debugPrint('You selected $selection');
+                Provider.of<MedicationProvider>(context, listen: false)
+                    .setSelectedPillName(selection);
+              },
+              fieldViewBuilder: (
+                  BuildContext context,
+                  TextEditingController fieldTextEditingController,
+                  FocusNode fieldFocusNode,
+                  VoidCallback onFieldSubmitted,
+                  ) {
+                // Add listener to text editing controller
+                fieldTextEditingController.addListener(() {
+                  final currentText = fieldTextEditingController.text;
+                  if (_medicationNames.contains(currentText)) {
+                    if (!_isValidSelection) {
+                      setState(() => _isValidSelection = true);
+                    }
+                  } else {
+                    if (_isValidSelection) {
+                      setState(() => _isValidSelection = false);
+                    }
+                  }
+                });
+
+                return CustomTextField(
+                  autofocus: true,
+                  controller: fieldTextEditingController,
+                  focusNode: fieldFocusNode,
+                  name: "Medication Name",
+                  prefixIcon: LineIcons.pills,
+                  inputType: TextInputType.text,
+                  onChanged: (value) {
+                    // This is no longer needed since we have the listener above
+                    // but if you want to keep it for other purposes, you can.
+                  },
+                );
+              },
+              optionsViewBuilder: (BuildContext context,
+                  AutocompleteOnSelected<String> onSelected,
+                  Iterable<String> options) {
+                return Align(
+                  alignment: Alignment.topLeft,
+                  child: Material(
+                    elevation: 4.0,
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: appTheme.grey700,
+                          borderRadius: BorderRadiusStyle.circleBorder29),
+                      width: 300,
+                      height: 200,
+                      child: ListView.builder(
+                        padding: EdgeInsets.all(10.0),
+                        itemCount: options.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final String t9Medication = options.elementAt(index);
+                          return ListTile(
+                              title: Text(t9Medication,
+                                  style: TextStyle(color: appTheme.grey100)),
+                              onTap: () => onSelected(t9Medication));
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            Visibility(
+              visible: _isValidSelection,
+              child: CustomElevatedButton(
+                onPressed: () => Navigator.pushNamed(
+                    context, AppRoutes.selectMedicationTypeScreen),
+                text: 'Next',
+                width: 100.h,
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
   }
 }
--- pillmate-v1.0.2/lib/widgets/custom_text_field.dart
+++ pillmate-v1.0.2/lib/widgets/custom_text_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../theme/theme_helper.dart';
 
 class CustomTextField extends StatelessWidget {
@@ -9,16 +8,22 @@ class CustomTextField extends StatelessWidget {
   final bool obscureText;
   final TextCapitalization textCapitalization;
   final TextInputType inputType;
+  final FocusNode? focusNode; // Existing focusNode parameter
+  final Function(String)? onChanged; // New onChanged callback parameter
+  final bool autofocus;
 
-  const CustomTextField(
-      {Key? key,
-      required this.controller,
-      required this.name,
-      required this.prefixIcon,
-      this.obscureText = false,
-      this.textCapitalization = TextCapitalization.none,
-      required this.inputType})
-      : super(key: key);
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.name,
+    required this.prefixIcon,
+    this.obscureText = false,
+    this.textCapitalization = TextCapitalization.none,
+    required this.inputType,
+    this.focusNode, // Existing focusNode initialization
+    this.onChanged,
+    this.autofocus = false, // Initialize onChanged
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,25 +34,31 @@ class CustomTextField extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(bottom: 15),
       child: TextField(
-          enabled: true,
-          controller: controller,
-          textCapitalization: textCapitalization,
-          maxLength: 40,
-          maxLines: 1,
-          obscureText: obscureText,
-          keyboardType: inputType,
-          textAlign: TextAlign.start,
-          style: TextStyle(color: appTheme.whiteA700, fontSize: 16),
-          decoration: InputDecoration(
-            prefixIcon: Icon(prefixIcon, color: appTheme.teal500),
-            isDense: true,
-            labelText: name,
-            counterText: "",
-            labelStyle: TextStyle(color: appTheme.cyan500),
-            border: outlineInputBorder,
-            focusedBorder: outlineInputBorder,
-            enabledBorder: outlineInputBorder,
-          )),
+        autofocus: autofocus,
+        enabled: true,
+        controller: controller,
+        focusNode: focusNode,
+        // Use the focusNode here
+        textCapitalization: textCapitalization,
+        maxLength: 40,
+        maxLines: 1,
+        obscureText: obscureText,
+        keyboardType: inputType,
+        textAlign: TextAlign.start,
+        style: TextStyle(color: appTheme.whiteA700, fontSize: 16),
+        onChanged: onChanged,
+        // Use the onChanged callback here
+        decoration: InputDecoration(
+          prefixIcon: Icon(prefixIcon, color: appTheme.teal500),
+          isDense: true,
+          labelText: name,
+          counterText: "",
+          labelStyle: TextStyle(color: appTheme.cyan500),
+          border: outlineInputBorder,
+          focusedBorder: outlineInputBorder,
+          enabledBorder: outlineInputBorder,
+        ),
+      ),
     );
   }
 }
